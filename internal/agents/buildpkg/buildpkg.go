54a0c01fbc9d01d6f0e17e2641b06761220e56af
Your Name <you@example.com>
2023-06-08 19:31:17 +0000

    j

--- internal/agents/buildpkg/buildpkg.go
+++ internal/agents/buildpkg/buildpkg.go
@@ -10,21 +10,6 @@ import (
 	"github.com/InfinityBotList/ibl/types"
 )
 
-const CrossCompileNote = `
-
--- MacOS cross compile --
-
-1. Follow https://stackoverflow.com/questions/40424255/cross-compilation-to-x86-64-unknown-linux-gnu-fails-on-mac-osx
-2. Use https://github.com/MaterializeInc/homebrew-crosstools for cross compiling as it is newer
-3. Update path: "PATH=/opt/homebrew/Cellar/x86_64-unknown-linux-gnu/0.1.0/bin:$PATH"
-
-## Not always needed, try running make cross before doing the below
-
-4. Symlink gcc if needed by ring at /opt/homebrew/Cellar/x86_64-unknown-linux-gnu/0.1.0/bin based on the error you get
-5. Replace 0.1.0 with whatever gcc version you need
-6. If you face any build issues on macOS, try removing /opt/homebrew/bin/x86_64-linux-gnu-gcc and then ln -sf /opt/homebrew/bin/x86_64-unknown-linux-gnu-cc /opt/homebrew/bin/x86_64-linux-gnu-gcc
-`
-
 const DefaultTarget = "x86_64-unknown-linux-gnu"
 
 // Internal struct
